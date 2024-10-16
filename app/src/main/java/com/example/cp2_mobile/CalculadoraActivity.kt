2defd13a24d0aaff2dbfd78bccda5bcaa67139aa
devluish <luis_unn@hotmail.com>
2024-10-16 19:25:26 -0300

    Commit

--- app/src/main/java/com/example/cp2_mobile/CalculadoraActivity.kt
+++ app/src/main/java/com/example/cp2_mobile/CalculadoraActivity.kt
@@ -1,28 +1,14 @@
 package com.example.cp2_mobile
 
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class CalculadoraActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculadora)
 
-        // Configura a seta de voltar
-        val setaVoltar = findViewById<ImageView>(R.id.seta_voltar)
-        setaVoltar.setOnClickListener {
-            // Apenas finaliza a CalculadoraActivity, voltando automaticamente para a Home
-            finish() // Finaliza a CalculadoraActivity e volta à HomeActivity
-        }
 
-        // Ajustes para as barras do sistema
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
--- app/src/main/java/com/example/cp2_mobile/TelefoneActivity.kt
+++ app/src/main/java/com/example/cp2_mobile/TelefoneActivity.kt
@@ -1,27 +1,12 @@
 package com.example.cp2_mobile
 
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class TelefoneActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_telefone)
 
-        // Configura a seta de voltar
-        val setaVoltar = findViewById<ImageView>(R.id.imageView)  // Usando o ID correto do layout
-        setaVoltar.setOnClickListener {
-            finish() // Finaliza a TelefoneActivity e volta à HomeActivity
-        }
-
-        // Ajustes para as barras do sistema
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
